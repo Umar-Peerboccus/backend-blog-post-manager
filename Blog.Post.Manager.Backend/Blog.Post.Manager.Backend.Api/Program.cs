17703388a035b876799a69e9ceac13ff082e37b1
Umar Mohammad Peerboccus <umar.m.peerboccus@accenture.com>
2025-03-27 13:48:06 +0400

    Revert "#016-add logger"
    This reverts commit 3010c82437dde593758a1bbe118f0387ad56dd3e.

--- Blog.Post.Manager.Backend/Blog.Post.Manager.Backend.Api/Program.cs
+++ Blog.Post.Manager.Backend/Blog.Post.Manager.Backend.Api/Program.cs
@@ -10,7 +10,6 @@ using FluentValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
@@ -28,87 +27,70 @@ public static class Program
     /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public static async Task Main(string[] args)
     {
-        var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger("Startup");
+        var builder = WebApplication.CreateBuilder(args);
 
-        try
+        // Configuration for Cosmos DB NoSql Database
+
+        // Bind settings
+        builder.Services.Configure<CosmosDbSettings>(builder.Configuration.GetSection("CosmosDb"));
+
+        // Register CosmosClient
+        builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            // Get the configuration from appsettings.json
+            var settings = serviceProvider.GetRequiredService<IOptions<CosmosDbSettings>>().Value;
+            return new CosmosClient(settings.AccountEndpoint, settings.Key);
+        });
 
-            // Configuration for Cosmos DB NoSql Database
+        // Register BlogPostStore
+        builder.Services.AddTransient<IBlogPostStore, BlogPostStore>();
 
-            // Bind settings
-            builder.Services.Configure<CosmosDbSettings>(builder.Configuration.GetSection("CosmosDb"));
-
-            // Register CosmosClient
-            builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
-            {
-                // Get the configuration from appsettings.json
-                logger.LogInformation("Get the configuration from app settings");
-
-                var settings = serviceProvider.GetRequiredService<IOptions<CosmosDbSettings>>().Value;
-                logger.LogInformation("Account Endpoint: " + settings.AccountEndpoint + " Key: " + settings.Key + " Database Name: " + settings.DatabaseName + " Container Name: " + settings.ContainerName);
-
-                return new CosmosClient(settings.AccountEndpoint, settings.Key);
-            });
-
-            // Register BlogPostStore
-            builder.Services.AddTransient<IBlogPostStore, BlogPostStore>();
-
-            // Configuration for MediatR
-            builder.Services.AddMediatR(cfg =>
-            {
-                cfg.RegisterServicesFromAssembly(typeof(GetAllBlogPostQueryHandler).Assembly);
-                cfg.RegisterServicesFromAssembly(typeof(CreateBlogPostCommandHandler).Assembly);
-            });
-
-            // Register AutoMapper with BusinessProfle.
-            builder.Services.AddAutoMapper(typeof(BusinessProfile).Assembly);
-
-            // Register FluentValidation
-            builder.Services.AddValidatorsFromAssembly(typeof(CreateBlogPostRequestModelValidator).Assembly);
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Blog Post Manager API",
-                    Version = "v1",
-                    Description = "Blog Post Manager Swagger API"
-                });
-            });
-
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            });
-
-            var app = builder.Build();
-            app.UseCors("AllowAll");
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog Post Manager API v1");
-                c.RoutePrefix = string.Empty; // Set Swagger UI at root URL
-                c.InjectStylesheet("/swagger-ui/custom.css"); // Helps debug UI loading issues
-            });
-
-            logger.LogInformation("Swagger configuration completed");
-
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
-            app.MapControllers();
-
-            logger.LogInformation("Starting app...");
-            await app.RunAsync();
-        }
-        catch (Exception ex)
+        // Configuration for MediatR
+        builder.Services.AddMediatR(cfg =>
         {
-            logger.LogError(ex, "An error occurred during startup.");
-            throw;
-        }
+            cfg.RegisterServicesFromAssembly(typeof(GetAllBlogPostQueryHandler).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(CreateBlogPostCommandHandler).Assembly);
+        });
+
+        // Register AutoMapper with BusinessProfle.
+        builder.Services.AddAutoMapper(typeof(BusinessProfile).Assembly);
+
+        // Register FluentValidation
+        builder.Services.AddValidatorsFromAssembly(typeof(CreateBlogPostRequestModelValidator).Assembly);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Blog Post Manager API",
+                Version = "v1",
+                Description = "Blog Post Manager Swagger API"
+            });
+        });
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll",
+                policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        });
+        
+        var app = builder.Build();
+        app.UseCors("AllowAll");
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog Post Manager API v1");
+            c.RoutePrefix = string.Empty; // Set Swagger UI at root URL
+            c.InjectStylesheet("/swagger-ui/custom.css"); // Helps debug UI loading issues
+        });
+
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.MapControllers();
+
+        await app.RunAsync();
     }
 }
